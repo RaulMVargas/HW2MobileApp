df8d5a6bc33a90e6ccf2dcd0eb9af03308b316d3
Raul Vargas <rvargas321@gmail.com>
2022-09-17 22:17:55 -0400

    initial commit

---
+++ app/src/main/java/com/example/invitationcard/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.invitationcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/***********************************************
+ Author: Raul Vargas
+ Date: 9/17/2022
+ Purpose: The purpose of this assignment was to create an invitation
+ screen and use the concept that we learned in class like margins
+ and padding in order to create a nice looking party invitation.
+ What Learned: I think I learned how to navigate the text properties
+ for the textviews and imageviews.
+ Sources of Help: None
+ Time Spent: 2 Hours
+ ***********************************************
+
+ ***********************************************
+Mobile App Development I -- COMP.4630 Honor Statement
+The practice of good ethical behavior is essential for
+maintaining good order in the classroom, providing an
+enriching learning experience for students, and training as
+a practicing computing professional upon graduation. This
+practice is manifested in the University's Academic
+Integrity policy. Students are expected to strictly avoid
+academic dishonesty and adhere to the Academic Integrity
+policy as outlined in the course catalog. Violations will
+be dealt with as outlined therein. All programming
+assignments in this class are to be done by the student
+alone unless otherwise specified. No outside help is
+permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is
+mine and was generated in a manner consistent with this
+document, the course academic policy on the course website
+on Blackboard, and the UMass Lowell academic code.
+Date:9/17/2022
+Name: Raul Vargas
+ */
